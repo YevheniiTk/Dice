ccd385fa7ff0b3b6f66fbc1defd13e6d8bf6027b
YevheniiTk <vkstrelok@gmail.com>
2018-03-13 19:41:26 +0200

    Add Dice

---
+++ Dice/Dice/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Dice
+{
+    public class Player
+    {
+        private int numberAttempts = 0;
+
+        public string Name { get; private set; }
+
+        public Player(string name, uint numberOfDices)
+        {
+            Name = name;
+
+            Game game = new Game(numberOfDices);
+
+            game.FellOutTwelve += () => Win();
+
+            while (true)
+            {
+                numberAttempts++;
+                game.StartGame();
+                Thread.Sleep(300);
+            }
+        }
+
+        public void Win()
+        {
+            Console.WriteLine($"{Name} - You win!!!");
+            Console.WriteLine($"Number of attempts is {numberAttempts}");
+            numberAttempts = 0;
+            Console.WriteLine($"Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
